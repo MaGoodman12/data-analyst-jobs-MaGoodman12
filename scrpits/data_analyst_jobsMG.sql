2565eb1cc04f927b65ff3362fb3417eea305b4cf
MaGoodman12 <magoody12@gmail.com>
2021-08-14 12:14:47 -0500

    Finished Project

--- scrpits/data_analyst_jobsMG.sql
+++ scrpits/data_analyst_jobsMG.sql
@@ -1,16 +1,16 @@
---1.How many rows are in the data_analyst_jobs table?
+--DONE 1.How many rows are in the data_analyst_jobs table?
 
 SELECT COUNT(*)
 FROM data_analyst_jobs;
 
--- 2.Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
+--DONE 2.Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
 
 SELECT *
 FROM data_analyst_jobs
 LIMIT 10;
---10th company name "exxonmobil"
+--'exxonmobil' 10th company
 
---3.How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+--DONE 3.How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
 SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE location = 'TN';
@@ -19,45 +19,104 @@ SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE location IN ('TN', 'KY');
 
---4.How many postings in Tennessee have a star rating above 4?
+--DONE 4.How many postings in Tennessee have a star rating above 4?
 SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE location = 'TN'
-AND star_rating > 3;
+AND star_rating >= 4;
 
---5.How many postings in the dataset have a review count between 500 and 1000?
+--DONE 5.How many postings in the dataset have a review count between 500 and 1000?
 SELECT *
 FROM data_analyst_jobs
 WHERE review_count between 500 AND 1000;
 
---6.Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
-SELECT location AS state, company, AVG(star_rating) AS avg_rating
+--DONE 6.Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
+SELECT location AS state,
+company,
+round(AVG(star_rating),2) AS avg_rating
 FROM data_analyst_jobs
 WHERE star_rating IS NOT NULL
 GROUP BY location, company
-ORDER BY avg_rating desc;
+ORDER BY avg_rating desc,
+location asc;
 
---7.Select unique job titles from the data_analyst_jobs table. How many are there?
-SELECT DISTINCT title, COUNT(title)
+--DONE 7.Select unique job titles from the data_analyst_jobs table. How many are there?
+WITH dist_title AS
+(SELECT distinct title,
+ company,
+ location
 FROM data_analyst_jobs
-GROUP BY title;
+WHERE star_rating IS NOT NULL
+GROUP BY company, location, title
+ORDER BY title asc)
 
---8.How many unique job titles are there for California companies?
+SELECT count(*)
+FROM dist_title;
 
-SELECT DISTINCT title, COUNT as count
+--DONE 8.How many unique job titles are there for California companies?
+WITH dist_title AS
+(
+SELECT distinct title
 FROM data_analyst_jobs
-WHERE location = 'CA'
+WHERE star_rating IS NOT NULL
+AND location = 'CA'
+GROUP BY company, location, title
+ORDER BY title asc
+)
+
+SELECT count(*)
+FROM dist_title;
+
+--DONE 9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+select company,
+round(avg(star_rating),2) as avg_rating,
+review_count
+from data_analyst_jobs
+where star_rating is not null AND
+company is not null AND
+review_count >= 5000
+group by company, review_count
+order by review_count desc;
+
+--DONE 10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+select company,
+round(avg(star_rating),2) as avg_rating,
+review_count
+from data_analyst_jobs
+where star_rating is not null AND
+company is not null AND
+review_count >= 5000
+group by company, review_count
+order by avg_rating desc, review_count desc;
+--'Kaiser Permanente' with 4.20 rounded avg star rating
+
+--DONE 11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+SELECT title,
+count(title)
+FROM data_analyst_jobs
+WHERE title ILIKE ('%nalyst%')
+GROUP BY title
+order by count desc;
+
+--DONE 12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT ILIKE ('%naly%')
 GROUP BY title;
+--All have "Tableau" in common
 
---9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
-
---10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
-
---11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
-
---12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
-
---BONUS.You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
+--DONE BONUS.You want to understand which jobs requiring SQL are hard to fill.
+--Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
 --Disregard any postings where the domain is NULL.
 --Order your results so that the domain with the greatest number of hard to fill jobs is at the top.
 --Which three industries are in the top 4 on this list?How many jobs have been listed for more than 3 weeks for each of the top 4?
+
+SELECT domain,
+COUNT(title)
+FROM data_analyst_jobs
+WHERE skills ILIKE ('%SQL%')
+AND days_since_posting >= 21
+AND domain IS NOT Null
+GROUP BY domain
+ORDER BY COUNT(title) DESC 
+LIMIT 4;
